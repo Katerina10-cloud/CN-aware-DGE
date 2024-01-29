3f317d88e3fa783579f83afc95ca60b37515e12e
Katerina10-cloud <katiasen89@gmail.com>
2024-01-29 09:24:06 +0100

    code refactoring

--- R scripts/data_analysis.R
+++ R scripts/data_analysis.R
@@ -9,11 +9,19 @@ library(DESeq2)
 #Exploration of CNV and RNAseq data 
 statRes_map_CNV = read.csv('~/model_fit_Python/model_results/results_luad/results_3/statRes_map_CNV.csv',header=TRUE)
 statRes_map_noCNV = read.csv('~/model_fit_Python/model_results/results_brca/statRes_map_noCNV.csv',header=TRUE)
-metadata = read.csv('model_fit_Python/model_data/metadata.csv',header=TRUE)
+rna_normal = read.csv('~/model_fit_Python/model_data/test_norm_46_luad/metadata.csv',header=TRUE)
+rna_normal = read.csv('~/model_data/TCGA/breast_cancer/sinthetic_data/rna_normal.csv',header=TRUE)
 
-statRes_map_noCNV <- statRes_map_noCNV %>% remove_rownames %>% column_to_rownames(var="GeneID")
-rna_counts <- rna_counts[(rownames(rna_counts) %in% rownames(res_allGenes)),]
+cnv_tumor <- brca_cnv_tumor %>% select(1:100)
+cnv <- cnv %>% remove_rownames %>% column_to_rownames(var="Row.names")
+cnv_tumor <- cnv_tumor[(rownames(cnv_tumor) %in% rownames(rna_normal)),]
 res_rna_cnv <- res_rna_cnv %>% relocate(padj_2, .after = padj_1)
+cnv <- cnv[,-1]
+
+cnv_tumor <- cnv_tumor %>% select(51:100)
+cnv <- merge(rna_n, cnv_tumor, by = "row.names")
+cnv_tumor <- replace(cnv, cnv>5, 5)
+rna_norm_tum <- cbind(rna_normal, rna_tumor)
 
 #res_allGenes$GeneID <- rownames(res_allGenes)
 #res_allGenes <- res_allGenes %>% mutate(difference = B1_2 - B1_1)
@@ -25,8 +33,8 @@ res_rna_cnv <- res_rna_cnv %>% relocate(padj_2, .after = padj_1)
 #cnv_3 <- cnv_brca %>% select(23,24,36)
 #rna_norm_3 <- brca_rna_norm %>% select(55,57,93)
 #rna_tum_3 <- brca_rna_tum %>% select(55,57,92)
-#save(metadata, file = "~/model_data/TCGA/lung_cancer/LUAD/metadata.Rdata")
-#write.csv(rna, file = "~/model_data/TCGA/lung_cancer/LUAD/rna_cnv_10.csv")
+save(rna, file = "~/model_data/TCGA/lung_cancer/sinthetic data/rna.Rdata")
+write.csv(rna_cnv, file = "~/model_data/TCGA/breast_cancer/sinthetic_data/heterog_cnv/rna_cnv.csv")
 
 #Data manipulation and cleaning
 stat_res_luad <- stat_res_luad %>% mutate(difference = B1_2 - B1_1)
@@ -60,15 +68,53 @@ cnv_normal <- data.frame("TCGA-50-5932-11A" = rep(1, nrow(luad_cnv)), "TCGA-44-6
                          "TCGA-50-5939-11A" = rep(1, nrow(luad_cnv)), "TCGA-50-5935-11A" = rep(1, nrow(luad_cnv)))
 
 #Metadata generation
-metadata <- data.frame(patID = colnames(luad_rna), 
-                       condition = rep(c("A", "B"), each = 10)) 
+rna_nor <- rna_normal %>% gsub("-", ".", rna_normal)
+metadata <- data.frame(patID = colnames(rna_norm_tum), 
+                       condition = rep(c("A", "B"), each = 50)) 
 metadata <- metadata %>% remove_rownames %>% column_to_rownames(var="patID")  
 metadata$condition <- as.factor(metadata$condition)
+all.equal(colnames(),rownames())
+
+#Generating simulated data
+#starting from normal mRNA counts
+# remove genes with low counts
+rna_normal <- rna_normal[rowSums(rna_normal) > 400,]
+
+
+rna_norm <- rna %>% select(1:50)
+rna_tum <- rna %>%  select(51:100)
+
+group1 <- rna_normal[1:2000,]
+group2 <- rna_normal[2001:4000,]
+group3 <- rna_normal[4001:6000,]
+group4 <- rna_normal[6001:8000,]
+group5 <- rna_normal[8001:10000,]
+
+cnv1 <- matrix(1, nrow(group1), 50)
+cnv2 <- matrix(2, nrow(group1), 50)
+cnv3 <- matrix(3, nrow(group1), 50)
+cnv4 <- matrix(4, nrow(group1), 50)
+cnv5 <- matrix(5, nrow(group1), 50)
+
+cnv <- rbind(cnv1, cnv2, cnv3, cnv4, cnv5)
+rna_normal <- rbind(group1, group2, group3, group4, group5)
+rna_tumor <- rna_tumor[(rownames(rna_tumor) %in% rownames(rna_normal)),]
+rna_norm_tum <- cbind(rna_norm, rna_tum)
+
+colnames(cnv_norm) <- colnames(rna_norm)
+rownames(cnv_norm) <- rownames(rna_norm)
+
+cnv_norm <- matrix(1, nrow(rna_tum), 50)
+cnv <- cbind(cnv_tumor, cnv_norm)
+cnv <- cbind(cnv_tum, cnv_norm)
 
 #Correction of normal RNAseq counts for CNV
-cnv <- cnv/2
+cnv_tum <- cnv
+cnv_tumor <- cnv_tumor/2
+rna_tum <- rna_tum * cnv_tumor
 cnv <- cnv + 10e-9
-rna <- luad_rna * cnv
+rna_tumor <- rna_tumor * cnv_tum
+rna_cnv <- rna_norm_tum * cnv
 
 #Counts normalization
 load("~/model_data/TCGA/lung_cancer/LUSC/cnv_lusc.Rdata")
