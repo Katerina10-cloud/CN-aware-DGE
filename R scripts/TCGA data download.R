af2e4be50900d0f399c31ed40e673cac3f52db22
Katsiaryna Davydzenka <katsiarynadavydzenka@MBPdiKatsiaryna.homenet.telecomitalia.it>
2024-04-26 23:15:27 +0200

    code update

--- R scripts/TCGA data download.R
+++ R scripts/TCGA data download.R
@@ -1,4 +1,6 @@
-#loading libraries
+### Download data from GDC portal ###
+
+
 #BiocManager::install("TCGAbiolinks")
 library(TCGAbiolinks)
 library(tidyverse)
@@ -9,10 +11,10 @@ setwd("/Users/katsiarynadavydzenka/Documents/PhD_AI/TCGA/")
 
 # Get a list of projects #
 gdcprojects = getGDCprojects()
-getProjectSummary('TCGA-OV')
+getProjectSummary('TCGA-GBM')
 
 # build a query to retrieve data #
-query_TCGA_cnv <- GDCquery(project = 'TCGA-LAML',
+query_TCGA_cnv <- GDCquery(project = 'TCGA-GBM',
         data.category = 'Copy Number Variation',
         #sample.type = "Primary Blood Derived Cancer - Peripheral Blood",
         data.type = "Gene Level Copy Number",
@@ -23,12 +25,12 @@ output_query_TCGA <- getResults(query_TCGA_cnv)
 
 
 # build a query to retrieve Tumor gene expression data #
-query_TCGA_rna <- GDCquery(project = 'TCGA-LAML',
+query_TCGA_rna <- GDCquery(project = 'TCGA-GBM',
                        data.category = 'Transcriptome Profiling',
                        experimental.strategy = 'RNA-Seq',
                        workflow.type = 'STAR - Counts',
                        data.type = "Gene Expression Quantification",
-                       #sample.type = "Primary Tumor",
+                       sample.type = "Primary Tumor",
                        access = 'open')
                        #barcode = luad_rna_tumor)
 
@@ -51,25 +53,24 @@ GDCdownload(clinical_luad)
 
 clinical_luad <- GDCprepare_clinic(clinical_luad, clinical.info = "patient")
 clinical_luad <- clinical_luad %>% select(1,6,7,69)
-save(clinical_luad, file = "~/model_data/TCGA/lung_cancer/LUAD/data/clinical_luad.Rdata")
+save(gbml_cnv_tumor, file = "gbm_cnv_tumor.Rdata")
 
 
 ### Prepare data ###
-laml_cnv <- GDCprepare(query_TCGA_cnv, summarizedExperiment = TRUE)
-laml_cnv_tumor <- assay(laml_cnv, 'copy_number', rownames = TRUE)
+gbm_cnv <- GDCprepare(query_TCGA_cnv, summarizedExperiment = TRUE)
+gbm_cnv_tumor <- assay(gbm_cnv, 'copy_number', rownames = TRUE) %>% as.data.frame()
 
-laml_rna <- GDCprepare(query_TCGA_rna, summarizedExperiment = TRUE)
-laml_rna_tumor <- assay(laml_rna, 'unstranded', rownames = TRUE)
+gbm_rna <- GDCprepare(query_TCGA_rna, summarizedExperiment = TRUE)
+gbm_rna_tumor <- assay(gbm_rna, 'unstranded', rownames = TRUE)
 
-gene_name <- as.data.frame(laml_rna@rowRanges@elementMetadata@listData[["gene_name"]]) 
+gene_name <- as.data.frame(gbm_rna@rowRanges@elementMetadata@listData[["gene_name"]]) 
 colnames(gene_name)[1] <- "GeneID"
-laml_rna_tumor <- as.data.frame(laml_rna_tumor)
-laml_rna_tumor <- cbind(gene_name, laml_rna_tumor)
-laml_rna_tumor <- laml_rna_tumor[!duplicated(laml_rna_tumor$GeneID), ] %>% remove_rownames %>% column_to_rownames(var="GeneID")
-laml_rna_tumor <- na.omit(laml_rna_tumor)
+gbm_rna_tumor <- cbind(gene_name, gbm_rna_tumor)
+gbm_rna_tumor <- gbm_rna_tumor[!duplicated(gbm_rna_tumor$GeneID), ] %>% remove_rownames %>% column_to_rownames(var="GeneID") %>%
+  na.omit()
 
 #substring columns
-colnames(laml_rna_tumor) <- substr(colnames(laml_rna_tumor), 1, 12)
+colnames(gbm_rna_tumor) <- substr(colnames(gbm_rna_tumor), 1, 12)
 
 
 save(laml_cnv_tumor, file = '~/Documents/PhD_AI/TCGA/aml_cancer/laml_cnv_tumor.Rdata')
@@ -83,8 +84,8 @@ clinical_brca <- GDCprepare_clinic(clinical_brca, clinical.info = "patient")
 clinical_brca <- clinical_brca %>% select(1,6,22,107)
 save(clinical_brca, file = "~/model_data/TCGA/breast_cancer/data/clinical_brca.Rdata")
 
-laml_rna_tumor <- laml_rna_tumor[(rownames(laml_rna_tumor) %in% rownames(res_allGenes)),]
-laml_rna_tumor <- laml_rna_tumor[,(colnames(laml_rna_tumor) %in% colnames(laml_cnv_tumor))]
+gbm_rna_tumor <- gbm_rna_tumor[(rownames(gbm_rna_tumor) %in% rownames(gbml_cnv_tumor)),]
+gbm_rna_tumor <- gbm_rna_tumor[,(colnames(gbm_rna_tumor) %in% colnames(gbml_cnv_tumor))]
 
 #rownames(countdata) <- seqdata[,1]
 #substr("ThisIsAString", start=1, stop=5) #shorten sample names
@@ -94,8 +95,8 @@ laml_rna_tumor <- laml_rna_tumor[,(colnames(laml_rna_tumor) %in% colnames(laml_c
 
 
 #Filtering low counts genes
-laml_rna_tumor <- laml_rna_tumor[which(rowSums(laml_rna_tumor)>100),]
-laml_cnv_tumor <- laml_cnv_tumor[(rownames(laml_cnv_tumor) %in% rownames(laml_rna_tumor)),] #delete rows by nam
+gbm_rna_tumor <- gbm_rna_tumor[which(rowSums(gbm_rna_tumor)>100),]
+gbm_rna_tumor <- laml_cnv_tumor[(rownames(laml_cnv_tumor) %in% rownames(laml_rna_tumor)),] #delete rows by nam
 
 
 
